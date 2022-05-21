c99f6fabdde6304addf6e7fdc7734023e1145427
kx0101 <liakos.koulaxis@yahoo.com>
2022-05-21 18:01:33 +0300

    added custom error handler

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ import express, { json, urlencoded } from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
 import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import { router as productsRoutes } from './routes/products/productsRoutes.js'
 import { connectDB } from './config/db.js';
 
 dotenv.config();
-const PORT = process.env.PORT || 5500;
 
 connectDB();
 
@@ -23,6 +23,11 @@ app.get("/", (req, res) => {
 })
 
 app.use('/api/products', productsRoutes)
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5500;
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`)
