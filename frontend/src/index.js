5659a88908d8f565e7e63322e2bf0b75870d91e8
kx0101 <liakos.koulaxis@yahoo.com>
2022-06-11 20:04:32 +0300

    deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import "./bootstrap.min.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./app/store.js";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
